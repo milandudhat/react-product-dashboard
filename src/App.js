818cf9190a653bfddb09cb32b34fc60f886812bf
Milan Dudhat <milandudhat2828@gmail.com>
2023-04-21 10:52:31 +0530

    React-product-dashboard

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, BrowserRouter , Routes } from 'react-router-dom';
 import Home from './components/Home';
-import Dashboard from './components/Dashboard';
 import AddProduct from './components/AddProduct';
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/addproduct" element={<AddProduct />} />
 
 
--- src/pages/Sidebar.js
+++ src/pages/Sidebar.js
@@ -107,26 +107,6 @@ export default function Sidebar() {
                             <ListItemText primary='Home' sx={{ opacity: open ? 1 : 0 }} />
                         </ListItemButton>
                     </ListItem>
-                    <ListItem disablePadding sx={{ display: 'block' }} onClick={()=>navigate('/dashboard')}>
-                        <ListItemButton
-                            sx={{
-                                minHeight: 48,
-                                justifyContent: open ? 'initial' : 'center',
-                                px: 2.5,
-                            }}
-                        >
-                            <ListItemIcon
-                                sx={{
-                                    minWidth: 0,
-                                    mr: open ? 3 : 'auto',
-                                    justifyContent: 'center',
-                                }}
-                            >
-                                <AutoGraphIcon />
-                            </ListItemIcon>
-                            <ListItemText primary='Dashboard' sx={{ opacity: open ? 1 : 0 }} />
-                        </ListItemButton>
-                    </ListItem>
                     <ListItem disablePadding sx={{ display: 'block' }} onClick={()=>navigate('/addproduct')}>
                         <ListItemButton
                             sx={{
